bfad4c26e28b373a51c33235d6cd0327f885a677
Nguyen Hoang Tuan <tuanmaiyeunguoido@gmail.com>
2024-08-07 09:55:35 +0700

    complete Contact page v1

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,8 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import { AiOutlineHome, AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { BiInfoCircle, BiPhoneCall } from "react-icons/bi";
 
 const Contact = () => {
   return (
@@ -20,6 +22,72 @@ const Contact = () => {
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-wrapper-inner d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title mb-4">Contact </h3>
+                  <form className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        className="form-control "
+                        type="text"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        className="form-control "
+                        type="email"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        className="form-control "
+                        type="tel"
+                        placeholder="Mobile"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols={"30"}
+                        rows={"4"}
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get in touch with uus</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineHome className="fs-5 mb-0"/>
+                        <address>135, Hai Ba Trung, District 1 , HCM city</address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5"/>
+                        <a href="tel:+84 377197374" className="text-dark">+84 377197374</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5"/>
+                        <a href="mailto:tuanxyz113@gmail.com" className="text-dark" >tuanxyz113@gmail.com</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiInfoCircle className="fs-5"/>
+                        <p className="mb-0">Monday - Friday 9 AM - 5 PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
